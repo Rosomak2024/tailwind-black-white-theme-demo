8249f3bd232267c660459af0886882b6c13bbfa6
Rosomak2024 <mbnet@o2.pl>
2025-12-25 17:22:13 +0100

    toggle classes added btn body header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const menu = document.querySelectorAll("li");
+
+button.addEventListener("click", () => {
+  body.classList.toggle("bg-white");
+  body.classList.toggle("bg-black");
+  header.classList.toggle("border-black");
+  header.classList.toggle("border-white");
+  menu.classList.toggle("text-white");
+  menu.classList.toggle("text-black");
+});
