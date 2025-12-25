49f6e1cf9d920d9eda8b3882820df63ea54064ed
Rosomak2024 <mbnet@o2.pl>
2025-12-25 17:44:11 +0100

    images added buttons toggle

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import "./style.css";
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 const header = document.querySelector("header");
-const menu = document.querySelectorAll("li");
 
 button.addEventListener("click", () => {
   body.classList.toggle("bg-white");
   body.classList.toggle("bg-black");
   header.classList.toggle("border-black");
   header.classList.toggle("border-white");
-  menu.classList.toggle("text-white");
-  menu.classList.toggle("text-black");
+  body.classList.toggle("text-black");
+  body.classList.toggle("text-white");
+  button.classList.toggle("text-white");
+  button.classList.toggle("text-black");
 });
