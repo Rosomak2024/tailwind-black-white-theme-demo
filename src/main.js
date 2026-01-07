2efde7a0662b90a53d33cc79ecfa2d80cb5ff23f
Rosomak2024 <mbnet@o2.pl>
2026-01-07 12:07:52 +0100

    change to dark-mode, classes,js, and style css for tailwind fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import "./style.css";
 
-const button = document.querySelector("button");
-const body = document.querySelector("body");
-const header = document.querySelector("header");
+const html = document.documentElement;
+const button = document.querySelector(".material-symbols-outlined");
 
 button.addEventListener("click", () => {
-  body.classList.toggle("bg-white");
-  body.classList.toggle("bg-black");
-  header.classList.toggle("border-black");
-  header.classList.toggle("border-white");
-  body.classList.toggle("text-black");
-  body.classList.toggle("text-white");
-  button.classList.toggle("text-white");
-  button.classList.toggle("text-black");
+  html.classList.toggle("dark");
 });
